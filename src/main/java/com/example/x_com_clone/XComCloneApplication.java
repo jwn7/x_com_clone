b9eb708f20be1f46ab46eb9a2bf76831e416ff14
jwn7 <ksyzing@naver.com>
2025-11-10 16:54:34 +0900

    초기 세팅

---
+++ src/main/java/com/example/x_com_clone/XComCloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.x_com_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XComCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XComCloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/x_com_clone/XComCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.x_com_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XComCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
